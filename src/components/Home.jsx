84d9163f68c0395b3fe3083bdf3e5a14f3d3d948
Haikal15 <haikalfajari156@gmail.com>
2023-06-03 15:58:54 +0700

    added user validation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,24 @@ import { Button } from "react-bootstrap";
 import { auth } from "../config/firebase";
 import DataList from "./DataList";
 import FormData from "./FormData";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
+  const [showDataList, setShowDataList] = useState(false);
 
   useEffect(() => {
     const notLogin = auth.onAuthStateChanged((user) => {
+      console.log(user);
       if (!user) {
         navigate("/login");
+      } else {
+        if (user.uid !== "IM48QsyBZ1ZCI3GfniaSD0M5gTq1") {
+          setShowDataList(false);
+        } else {
+          setShowDataList(true);
+        }
       }
     });
 
@@ -30,7 +38,7 @@ function Home() {
       <h1>Form Keluhan</h1>
       <FormData />
       <hr />
-      <DataList />
+      {showDataList && <DataList />}
     </>
   );
 }
